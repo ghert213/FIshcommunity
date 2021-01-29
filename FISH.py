b8e0c556b825a4c6c9584291db9a3675af300d39
ghert213 <73192899+ghert213@users.noreply.github.com>
2021-01-29 13:27:10 +0300

    Update FISH.py

--- FISH.py
+++ FISH.py
@@ -4,21 +4,11 @@ import os
 
 bot = telebot.TeleBot('1474693090:AAFs0giKJPrzCqk5kpbZlTyKZn7M1hxQ6gw')
 
-@bot.message_handler(content_types=['text', 'document', 'audio'])
-
-def start_message(message):
-	
-
-	
-	bot.send_message(message.chat.id, os.getcwd())
-	
-	bot.send_message(message.chat.id, os.listdir(os.getcwd()))
-	
-	
 
 
 
 bot.polling()
+
 
 
 
